fe0291bf3cc33c42c770ab84ea4c43fc4131a4f9
nalchamp <nicholasalym@gmail.com>
2024-03-10 00:47:49 -0500

    server fixed

--- server/server.js
+++ server/server.js
@@ -7,51 +7,14 @@ const db = new sqlite3.Database('./Resources/StarWarsdatabase.db');
 // Configure express to serve static files from the `public` directory.
 app.use(express.static('public'));
 
-/**
- * Fetches all the vehicles in the database.
- */
-app.get('/vehicleName', (req, res) => {
-    console.log(`Running handler for ${req.url}`)
-
-    console.log(`Querying database for vehicles data.`);
-    const query = `
-        SELECT DISTINCT(name)
-        FROM vehicles
-        ORDER BY name ASC;`
-    db.all(query, (err, rows) => {
-        if (err) {
-            console.error('Database error:', err);
-            res.status(500).json({ error: 'Database error' });
-            return;
-        }
-
-        const allVehicleName = [];
-        for (const row of rows) {
-            allVehicleName.push(row["name"]);
-        }
-
-        // Send the data as JSON in the response
-        res.json({ data: allVehicleName });
-    });
-});
-
-
-  // Requires there to be a `planets` query paramter.
-app.get('/vehicleSpeed', (req, res) => {
+  // Requires there to be a `speed` query paramter.
+app.get('/vehicle/speed', (req, res) => {
     console.log(`Running handler for ${req.url}`)
     // Extract and validate the request paramters.
-    const name = req.query.vehicleName;    
-    if (!name) {
-      res.status(400).json({ error: 'Expected vehicles name query paramter to be populated'});
-      return;
-    }
-
-    console.log(`Querying database for population data. vehicleName=${name}`);
     const query = `
-        SELECT max_atmosphering_speed
-        FROM vehicles
-        WHERE name = :name;`
-    db.all(query, { ":name": name }, (err, rows) => {
+        SELECT name, max_atmosphering_speed
+        FROM vehicles`
+    db.all(query, (err, rows) => {
         if (err) {
             console.error('Database error:', err);
             res.status(500).json({ error: 'Database error' });
@@ -60,8 +23,7 @@ app.get('/vehicleSpeed', (req, res) => {
 
         // Send the data as JSON in the response
         res.json({
-            name,
-            data: rows,
+            data: rows
         });
     });
 });
